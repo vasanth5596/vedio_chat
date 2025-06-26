fcde39ff6641622a2c5b14d11bf7ff527c3c1f1e
Vasanth <84504615+vasanth5596@users.noreply.github.com>
2025-06-26 18:00:35 +0530

    Update deploy.sh to use AWS ECR

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,18 @@ set -e  # Exit on error
 
 echo "⏱️ Starting deployment..."
 
+ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+ECR_REPOSITORY="video_chat"
+IMAGE_TAG="latest"
+
 echo "🚫 Stopping old container (if any)..."
 docker stop vedio_chat_container || true
 docker rm vedio_chat_container || true
 
-echo "🐳 Building Docker image..."
-docker build -t vedio_chat .
-
-echo "🏷️ Tagging image for Docker Hub..."
-docker tag vedio_chat vasanth5596/vedio_chat:latest
-
-echo "📤 Pushing image to Docker Hub..."
-docker push vasanth5596/vedio_chat:latest
+echo "📥 Pulling image from AWS ECR..."
+docker pull ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
 
 echo "🚀 Running the container..."
-docker run -d -p 5000:5000 --name vedio_chat_container vasanth5596/vedio_chat:latest
+docker run -d -p 5000:5000 --name vedio_chat_container ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
 
 echo "✅ Deployment completed and container running on port 5000"
