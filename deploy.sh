ebca0b9d6baa0716b3b8f2e4b3f4f99fbfba8297
Vasanth <84504615+vasanth5596@users.noreply.github.com>
2025-06-26 18:03:18 +0530

    Update deploy.sh to deploy to EC2 instance

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,12 @@ ECR_REPOSITORY="video_chat"
 IMAGE_TAG="latest"
 
 echo "🚫 Stopping old container (if any)..."
-docker stop vedio_chat_container || true
-docker rm vedio_chat_container || true
+ssh -i "your-key.pem" ec2-user@184.72.207.65 "docker stop vedio_chat_container || true && docker rm vedio_chat_container || true"
 
 echo "📥 Pulling image from AWS ECR..."
-docker pull ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+ssh -i "your-key.pem" ec2-user@184.72.207.65 "docker pull ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}"
 
 echo "🚀 Running the container..."
-docker run -d -p 5000:5000 --name vedio_chat_container ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+ssh -i "your-key.pem" ec2-user@184.72.207.65 "docker run -d -p 5000:5000 --name vedio_chat_container ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}"
 
 echo "✅ Deployment completed and container running on port 5000"
